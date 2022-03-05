39122c7367be810c42ab69d0a26064c31f10930c
EdTengHR <edmundteng988@gmail.com>
2022-03-06 00:54:54 +0800

    Updated solution

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -33,17 +33,31 @@ public class Folder implements Comparable<Folder>{
 	}
 	
 	public List<Note> searchNotes(String keywords) {
+		List<Note> intermediate = new ArrayList<Note>();
 		List<Note> output = new ArrayList<Note>();
 		ArrayList<Integer> orLocations = new ArrayList<Integer>();
+		ArrayList<Integer> optionalKeyIndex = new ArrayList<Integer>();		
+		ArrayList<Integer> requiredKeyIndex= new ArrayList<Integer>();		// Indices for required keywords
 		List<String> tokens = Arrays.asList(keywords.split(" "));
 		
 		// Go through list and identify the positions of the ORs
 		for (int i = 0; i < tokens.size(); i++) {
 			if (tokens.get(i).equalsIgnoreCase("or")) {
 				orLocations.add(i);
+				optionalKeyIndex.add(i-1);
+				optionalKeyIndex.add(i+1);
 			}
 		}
 		
+		for (int i = 0; i < tokens.size(); i++) {
+			if (!orLocations.contains(i) && !optionalKeyIndex.contains(i)) {
+				// For any values of i that aren't in the "or" index list or the optional keys list
+				// These values would be the indices for the required keywords
+				requiredKeyIndex.add(i);
+			}
+		}		
+		
+		// First handle ORs
 		for (Note n : notes) {
 			for (int j = 0; j < orLocations.size(); j ++) {
 				if (!(n.getTitle().toLowerCase().contains(tokens.get(orLocations.get(j)-1).toLowerCase()))
@@ -59,12 +73,31 @@ public class Folder implements Comparable<Folder>{
 					}
 				}
 				if (j == orLocations.size() - 1) {
-					output.add(n);
+					intermediate.add(n);
 				}
 			}
 		}
 		
-		return output;
+		// Then handle required keywords (ANDs)
+		for (Note o : intermediate) {
+			for (int j = 0; j < requiredKeyIndex.size(); j++) {
+				if (!(o.getTitle().toLowerCase().contains(tokens.get(requiredKeyIndex.get(j))))) {
+					if (o instanceof TextNote) {
+						if (!(((TextNote) o).getContent().toLowerCase().contains(tokens.get(requiredKeyIndex.get(j)).toLowerCase()))) {
+							break;
+						}
+					}
+					else {
+						break;
+					}
+				}
+				if (j == requiredKeyIndex.size() - 1) {
+					output.add(o);
+				}
+			}
+		}
+		
+		return (output.isEmpty()) ? intermediate : output;
 	}
 	
 	@Override
